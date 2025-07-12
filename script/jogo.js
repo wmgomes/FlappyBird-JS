ca82b3f9f626e9a33a246a634ed5af5ec41a27cd
Wellington Augusto de Melo Gomes <wellington.gomes@pixeon.com>
2025-07-12 07:52:42 -0300

    Ação de click, retorno para tela inicial e efeitos sonoros

--- script/jogo.js
+++ script/jogo.js
@@ -1,5 +1,8 @@
 console.log('Flappy Bird - Wellington');
 
+const som_HIT = new Audio();
+som_HIT.src = '../efeitos/hit.wav';
+
 const sprites = new Image();
 sprites.src = '../img/sprites.png';
 
@@ -63,37 +66,12 @@ const chao = {
     }
 }
 
-// [Bird]
-const flappyBird = {
-    spriteX: 0,
-    spriteY: 0,
-    largura: 33,
-    altura: 24,
-    x: 10,
-    y: 50,
-    gravidade: 0.25,
-    velocidade: 0,
-    atualiza() {
-        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
-        flappyBird.y = flappyBird.y + flappyBird.velocidade;
-    },
-    desenha() {
-        contexto.drawImage(
-            sprites,
-            flappyBird.spriteX, flappyBird.spriteY,
-            flappyBird.largura, flappyBird.altura,
-            flappyBird.x, flappyBird.y,
-            flappyBird.largura, flappyBird.altura
-        );
-    }
-}
-
 // [Chão]
 const mensagemGetReady = {
     spriteX: 134,
     spriteY: 0,
     largura: 174,
-    altura: 112,
+    altura: 153,
     x: (canvas.width / 2) - 174 / 2,
     y: 50,
     desenha() {
@@ -107,19 +85,71 @@ const mensagemGetReady = {
     }
 }
 
+// [Bird]
+function criaFlappyBird() {
+    const flappyBird = {
+        spriteX: 0,
+        spriteY: 0,
+        largura: 33,
+        altura: 24,
+        x: 10,
+        y: 50,
+        gravidade: 0.25,
+        velocidade: 0,
+        pulo: 4.6,
+        pula() {
+            this.velocidade = - this.pulo;
+        },
+        atualiza() {
+            if (fazColisao(flappyBird, chao)) {
+                som_HIT.play();
+
+                setTimeout(() => {
+                    mudaParaTela(Telas.INICIO);
+                }, 500);
+
+                return;
+            }
+
+            flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+            flappyBird.y = flappyBird.y + flappyBird.velocidade;
+        },
+        desenha() {
+            contexto.drawImage(
+                sprites,
+                flappyBird.spriteX, flappyBird.spriteY,
+                flappyBird.largura, flappyBird.altura,
+                flappyBird.x, flappyBird.y,
+                flappyBird.largura, flappyBird.altura
+            );
+        }
+    }
+
+    return flappyBird;
+}
+
 // [Telas]
+const globais = {};
+
 let telaAtiva = {};
 
 function mudaParaTela(novaTela) {
     telaAtiva = novaTela;
+
+    if (telaAtiva.inicializa) {
+        telaAtiva.inicializa();
+    }
 }
 
 const Telas = {
     INICIO: {
+        inicializa() {
+            globais.flappyBird = criaFlappyBird();
+        },
         desenha() {
             planoDeFundo.desenha();
             chao.desenha();
-            flappyBird.desenha();
+            globais.flappyBird.desenha();
             mensagemGetReady.desenha();
         },
         click() {
@@ -135,10 +165,13 @@ Telas.JOGO = {
     desenha() {
         planoDeFundo.desenha();
         chao.desenha();
-        flappyBird.desenha();
-    }, 
+        globais.flappyBird.desenha();
+    },
+    click() {
+        globais.flappyBird.pula();
+    },
     atualiza() {
-        flappyBird.atualiza();
+        globais.flappyBird.atualiza();
     }
 };
 
@@ -158,3 +191,15 @@ window.addEventListener('click', function() {
 
 mudaParaTela(Telas.INICIO);
 loop();
+
+function fazColisao(flappyBird, chao) {
+    const flappyBirdY = flappyBird.y + flappyBird.altura;
+    const chaoY = chao.y;
+
+    if (flappyBirdY >= chaoY) {
+        return true;
+    }
+
+    return false;
+}
+
